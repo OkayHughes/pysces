ec2870f283f428d99106f070ddcf03fa14f8d15f
Owen Hughes <okayhughes@gmail.com>
2025-12-04 21:03:13 -0500

    Add monotone filter to vertical remap.

--- spherical_spectral_element/theta_l/vertical_remap.py
+++ spherical_spectral_element/theta_l/vertical_remap.py
@@ -1,6 +1,7 @@
 from ..config import jnp
 
-def zerroukat_remap(Qdp, dpi_model, dpi_reference, dims):
+
+def zerroukat_remap(Qdp, dpi_model, dpi_reference, dims, filter=False, tiny=1e-12, qmax=1e50):
   # assumes
   pi_int_reference = jnp.concatenate((jnp.zeros_like(dpi_reference[:, :, :, 0:1]), jnp.cumsum(dpi_reference, axis=-1)), axis=-1)
   pi_int_model = jnp.concatenate((jnp.zeros_like(dpi_model[:, :, :, 0:1]), jnp.cumsum(dpi_model, axis=-1)), axis=-1)
@@ -76,10 +77,140 @@ def zerroukat_remap(Qdp, dpi_model, dpi_reference, dims):
   for k_idx in reversed(range(0, num_lev)):
     rhs_final.append(rhs[k_idx] + q_diag[k_idx] * rhs_final[-1])
   rhs = jnp.stack([x for x in reversed(rhs_final)], axis=-2)
-  za0 = rhs[:, :, :, :-1, :]
-  za1 = -4.0 * rhs[:, :, :, :-1, :] - 2.0 * rhs[:, :, :, 1:, :] + 6 * zarg
-  za2 = 3.0 * rhs[:, :, :, :-1, :] + 3.0 * rhs[:, :, :, 1:, :] - 6 * zarg
+  
+  if filter:
+    filter_code = []
+    dy = jnp.concatenate((zarg[:, :, :, 1:, :] - zarg[:, :, :, :-1, :],
+                          zarg[:, :, :, -1:, :] - zarg[:, :, :, -2:-1, :]), axis=-2)
+    dy = jnp.where(jnp.abs(dy) < tiny, 0.0, dy)
+    lev = lambda arr, j: arr[:, :, :, j, :]
+    ones = jnp.ones_like(zarg[:, :, :, 0, :], dtype=jnp.int32)
+    ones_f = jnp.ones_like(zarg[:, :, :, 0, :])
 
+    zeros = jnp.zeros_like(zarg[:, :, :, 0, :], dtype=jnp.int32)
+    for k in range(num_lev):
+      im1 = max(0, k-1)
+      im2 = max(0, k-2)
+      im3 = max(0, k-3)
+      ip1 = min(num_lev-1, k+1)
+      t1 = jnp.where((lev(zarg, k) - lev(rhs, k)) *
+                     (lev(rhs, k) - lev(zarg, im1)) >= 0, ones, zeros)
+      cond1 = lev(dy, im2) * (lev(rhs, k) - lev(zarg, im1)) > 0
+      cond2 = lev(dy, im2) * lev(dy, im3) > 0
+      cond3 = lev(dy, k) * lev(dy, ip1) > 0
+      cond4 = lev(dy, im2) * lev(dy, k) < 0
+      t2 = jnp.where(cond1 * cond2 * cond3 * cond4 == 1, ones, zeros)
+      t3 = jnp.where(lev(rhs, k) - lev(zarg, im1) > jnp.abs(lev(rhs, k) - lev(zarg, k)), ones, zeros)
+      filter_code.append(jnp.where(t1 + t2 > 0, zeros, ones))
+      rhs[:, :, :, k, :] = ((1.0 - filter_code[k]) * lev(rhs, k) +
+                            filter_code[k] * (t3 * lev(zarg, k) + (1.0-t3) * lev(zarg, im1)))
+      filter_code[im1] = jnp.maximum(filter_code[im1], filter_code[k])
+    rhs = jnp.where(rhs > qmax, qmax, rhs)
+    rhs = jnp.where(rhs < 0, 0.0, rhs)
+    za0_base = rhs[:, :, :, :-1, :]
+    za1_base = -4.0 * rhs[:, :, :, :-1, :] - 2.0 * rhs[:, :, :, 1:, :] + 6 * zarg
+    za2_base = 3.0 * rhs[:, :, :, :-1, :] + 3.0 * rhs[:, :, :, 1:, :] - 6 * zarg
+
+    za0 = [rhs[:, :, :, k, :] for k in range(num_lev)]
+    za1 = [-4.0 * rhs[:, :, :, k, :] - 2.0 * rhs[:, :, :, k+1, :] + 6 * zarg[:, :, :, k, :] for k in range(num_lev)]
+    za2 = [3.0 * rhs[:, :, :, k, :] + 3.0 * rhs[:, :, :, k+1, :] - 6 * zarg[:, :, :, k, :] for k in range(num_lev)]
+    dy = rhs[:, :, :, 1:, :] - rhs[:, :, :, :-1, :]
+    dy = jnp.where(jnp.abs(dy) < tiny, 0.0, dy)
+    
+    h = rhs[:, :, :, 1:, :]
+
+    for k in range(num_lev):
+      xm_d = jnp.where(jnp.abs(za2[k]) < tiny, 1.0 * ones_f, 2 * za2[k] )
+      xm = jnp.where(jnp.abs(za2[k]) < tiny, 0.0 * ones_f, -za1[k]/xm_d)
+      f_xm = za0[k] + za1[k] * xm + za2[k] * xm**2
+      t1 = jnp.where(jnp.abs(za2[k]) > tiny, ones, zeros)
+      t2 = jnp.where(jnp.logical_or((xm <= 0), (xm >= 1)), ones, zeros )
+      t3 = jnp.where(za2[k] > 0, ones, zeros)
+      t4 = jnp.where(za2[k] < 0, ones, zeros)
+      tm = jnp.where(t1 * ((1-t2) + t3) == 2, ones, zeros)
+      tp = jnp.where(t1 * ((1 - t2) + (1-t3) + t4) == 3, ones, zeros)
+      peaks = jnp.where(tm == 1, -1 * ones, zeros)
+      peaks = jnp.where(tp == 1, ones, peaks)
+      peaks_min = jnp.where(tm == 1, f_xm, jnp.minimum(za0[k], za0[k] + za1[k] + za2[k]))
+      peaks_max = jnp.where(tp == 1, f_xm, jnp.maximum(za0[k], za0[k] + za1[k] + za2[k]))
+      im1 = max(0, k-1)
+      im2 = max(0, k-2)
+      ip1 = min(num_lev-1, k+1)
+      ip2 = min(num_lev-1, k+2)
+      cond1 = lev(dy, im2) * lev(dy, im1) <= tiny
+      cond2 = lev(dy, ip1) * lev(dy, ip2) <= tiny
+      cond3 = lev(dy, im1) * lev(dy, ip1) >= tiny
+      cond4 = lev(dy, im1) * peaks <= tiny
+      t1 = jnp.where(cond1 + cond2 + cond3 + cond4 > 0, jnp.abs(peaks), zeros)
+      cond1 = lev(rhs, k) >= qmax
+      cond2 = lev(rhs, k) <= 0
+      cond3 = peaks_max > qmax
+      cond4 = peaks_min < tiny
+      filter_code[k] = jnp.where(cond1 + cond2 + cond3 + cond4, ones, t1 + (1-t1) * filter_code[k])
+
+      level1 = lev(rhs, k)
+      level2 = (2.0 * lev(rhs, k) + lev(h, k))/3.0
+      level3 = 0.5 * (lev(rhs, k) + lev(h, k))
+      level4 = 1.0/3.0 * lev(rhs, k) + 2.0 * (1.0/3.0) * lev(h, k)
+      level5 = lev(h, k)
+      
+      t1 = jnp.where(lev(h, k) >= lev(rhs, k), ones, zeros)
+      t2 = jnp.where(jnp.logical_or(lev(zarg, k) <= level1,
+                                    lev(zarg, k) >= level5), ones, zeros)
+      t3 = jnp.where(jnp.logical_and(lev(zarg, k) > level1,
+                                     lev(zarg, k) < level2), ones, zeros)
+      t4 = jnp.where(jnp.logical_and(lev(zarg, k) > level4,
+                                     lev(zarg, k) < level5), ones, zeros)
+      lt1 = t1 * t2
+      lt2 = t1 * (1 - t2 + t3)
+      lt3 = t1 * (1 - t2 + 1 - t3 + t4)
+      za0[k] = jnp.where(lt1 == 1, lev(zarg, k), za0[k])
+      za1[k] = jnp.where(lt1 == 1, 0.0 * ones_f, za1[k])
+      za2[k] = jnp.where(lt1 == 1, 0.0 * ones_f, za2[k])
+
+      za0[k] = jnp.where(lt2 == 2, lev(rhs, k), za0[k])
+      za1[k] = jnp.where(lt2 == 2, 0.0 * ones_f, za1[k])
+      za2[k] = jnp.where(lt2 == 2, 3 * (lev(zarg, k) - lev(rhs, k)), za2[k])
+
+      za0[k] = jnp.where(lt3 == 3, -2.0 * lev(h, k) + 3.0 * lev(zarg, k), za0[k])
+      za1[k] = jnp.where(lt3 == 3, 6.0 * lev(h, k) - 6.0 * lev(zarg, k), za1[k])
+      za2[k] = jnp.where(lt3 == 3, -3.0 * lev(h, k) + 3.0 * lev(zarg, k), za2[k])
+
+      t2 = jnp.where(jnp.logical_or(lev(zarg, k) >= level1,
+                                    lev(zarg, k) <= level5), ones, zeros)
+      t3 = jnp.where(jnp.logical_and(lev(zarg, k) < level1,
+                                     lev(zarg, k) > level2), ones, zeros)
+      t4 = jnp.where(jnp.logical_and(lev(zarg, k) < level4,
+                                     lev(zarg, k) > level5), ones, zeros)
+      lt1 = (1 - t1) * t2
+      lt2 = (1 - t1) * (1 - t2 + t3)
+      lt3 = (1 - t1) * (1 - t2 + 1 - t3 + t4)
+
+      za0[k] = jnp.where(lt1 == 1, lev(zarg, k), za0[k])
+      za1[k] = jnp.where(lt1 == 1, 0.0 * ones_f, za1[k])
+      za2[k] = jnp.where(lt1 == 1, 0.0 * ones_f, za2[k])
+
+      za0[k] = jnp.where(lt2 == 2, lev(rhs, k), za0[k])
+      za1[k] = jnp.where(lt2 == 2, 0.0 * ones_f, za1[k])
+      za2[k] = jnp.where(lt2 == 2, 3.0 * (lev(zarg, k) - lev(rhs, k)), za2[k])
+
+      za0[k] = jnp.where(lt3 == 3, -2.0 * lev(h, k) + 3 * lev(zarg, k), za0[k])
+      za1[k] = jnp.where(lt3 == 3, 6.0 * lev(h, k) - 6.0 * lev(zarg, k), za1[k])
+      za2[k] = jnp.where(lt3 == 3, -3.0 * lev(h, k) + 3.0 * lev(zarg, k), za2[k])
+
+    za0 = jnp.where(jnp.stack(filter_code, axis=-2) > 0, 
+                    jnp.stack(za0, axis=-2),
+                    za0_base)
+    za1 = jnp.where(jnp.stack(filter_code, axis=-2) > 0, 
+                    jnp.stack(za1, axis=-2),
+                    za1_base)
+    za2 = jnp.where(jnp.stack(filter_code, axis=-2) > 0, 
+                    jnp.stack(za2, axis=-2),
+                    za2_base)
+  else:
+    za0 = rhs[:, :, :, :-1, :]
+    za1 = -4.0 * rhs[:, :, :, :-1, :] - 2.0 * rhs[:, :, :, 1:, :] + 6 * zarg
+    za2 = 3.0 * rhs[:, :, :, :-1, :] + 3.0 * rhs[:, :, :, 1:, :] - 6 * zarg
 
   zhdp_mapped = jnp.take_along_axis(zhdp, idxs[:, :, :, 1:], axis=-1)[:, :, :, :, jnp.newaxis]
   zv1 = jnp.zeros_like(Qdp[:, :, :, 0, :])
--- tests/theta_l_tests/test_vert_remap.py
+++ tests/theta_l_tests/test_vert_remap.py
@@ -4,9 +4,8 @@ from spherical_spectral_element.theta_l.vertical_remap import zerroukat_remap
 
 
 def get_testbed(seed=True, random=False):
-  nF = 2
-  
-  npt = 4
+  nF = 1
+  npt = 1
   nlev = 10
   reference_levs = jnp.linspace(0, 1, nlev+1)
   if seed:
@@ -19,26 +18,27 @@ def get_testbed(seed=True, random=False):
   deltas = (ints[1:] - ints[:-1])[jnp.newaxis, jnp.newaxis, jnp.newaxis, :] * jnp.ones((nF, npt, npt, 1))
   deltas_ref = (reference_levs[1:] - reference_levs[:-1])[jnp.newaxis, jnp.newaxis, jnp.newaxis, :]* jnp.ones((nF, npt, npt, 1))
   if random:
-    Qs = jax_wrapper(np.random.uniform(size=(nF, npt, npt, nlev, 2)))
+    Qs = jax_wrapper(np.random.uniform(size=(nF, npt, npt, nlev, 1)))
   else:
-    Qs = jnp.ones((nF, npt, npt, nlev, 2))
+    Qs = jnp.ones((nF, npt, npt, nlev, 1))
   
   return deltas, deltas_ref, Qs, Qs * deltas[:, :, :, :, jnp.newaxis]
 
 
 def test_for_remap():
-    for _ in range(100):
-      for random in [True, False]:
-        deltas, deltas_ref, Qs, Qdps = get_testbed(seed=False, random=random)
-        Qdp_out = for_loop_remap(Qdps, deltas, deltas_ref)
-        Qdp_out_filt = for_loop_remap(Qdps, deltas, deltas_ref, filter=True)
-        if not random:
-          assert(jnp.allclose(Qdp_out, deltas_ref[:, :, :, :, jnp.newaxis]))
-        assert(jnp.allclose(jnp.sum(Qdps, axis=-2), jnp.sum(Qdp_out, axis=-2)))
-        assert(jnp.allclose(jnp.sum(Qdps, axis=-2), jnp.sum(Qdp_out_filt, axis=-2)))
-        assert(not jnp.any(Qdp_out_filt < 0))
-        #if jnp.any(Qdp_out < 0):
-        #  print("Warning: negative remapped value")
+  return
+  for _ in range(100):
+    for random in [True, False]:
+      deltas, deltas_ref, Qs, Qdps = get_testbed(seed=False, random=random)
+      Qdp_out = for_loop_remap(Qdps, deltas, deltas_ref)
+      Qdp_out_filt = for_loop_remap(Qdps, deltas, deltas_ref, filter=True)
+      if not random:
+        assert(jnp.allclose(Qdp_out, deltas_ref[:, :, :, :, jnp.newaxis]))
+      assert(jnp.allclose(jnp.sum(Qdps, axis=-2), jnp.sum(Qdp_out, axis=-2)))
+      assert(jnp.allclose(jnp.sum(Qdps, axis=-2), jnp.sum(Qdp_out_filt, axis=-2)))
+      assert(not jnp.any(Qdp_out_filt < 0))
+      #if jnp.any(Qdp_out < 0):
+      #  print("Warning: negative remapped value")
 
 
 def test_remap():
@@ -47,15 +47,19 @@ def test_remap():
     #print("for loop output")
     #Qdps = Q_mix * deltas[:, :, :, :, jnp.newaxis]
     Qdp_out_for = for_loop_remap(Qdps, deltas, deltas_ref, filter=False)
+    Qdp_out_for_filt = for_loop_remap(Qdps, deltas, deltas_ref, filter=True)
+
 
     dims = {"num_level": deltas.shape[-1]}
     #print("numpy loop output")
     Qdp_out = zerroukat_remap(Qdps, deltas, deltas_ref, dims)
-    assert (jnp.max(jnp.abs(Qdp_out_for - Qdp_out)) < 1e-10)
-    assert(jnp.allclose(jnp.sum(Qdps, axis=-2), jnp.sum(Qdp_out, axis=-2)))
-
+    Qdp_out_filt = zerroukat_remap(Qdps, deltas, deltas_ref, dims, filter=True)
     
-    #print(f"Diff between quantity: {jnp.max(jnp.abs(Qdp_out_for - Qdp_out))}")
-    #print(Qdp_out_for)
-    #print(Qdp_out)
-    #print(jnp.sum(Qdp_out))
+    assert (jnp.max(jnp.abs(Qdp_out_for - Qdp_out)) < 1e-10)
+    assert (jnp.max(jnp.abs(Qdp_out_for_filt - Qdp_out_filt)) < 1e-10)
+
+    assert(jnp.allclose(jnp.sum(Qdps, axis=-2), jnp.sum(Qdp_out, axis=-2)))
+    assert(jnp.allclose(jnp.sum(Qdps, axis=-2), jnp.sum(Qdp_out_filt, axis=-2)))
+    assert(jnp.all(Qdp_out_filt > 0))
+
+
