06fa68e379d3ff477b3b795ce6f91aa5fb3db7a7
Owen Hughes <okayhughes@gmail.com>
2025-12-09 12:01:14 -0500

    Fixes multiple bugs in time stepping. Firstly: advance_state was completely wrong, as the base value of the fold was not handled correctly. Secondly: refactor of lower boundary correction function (renamed to `correct_state`) was incomplete. Time stepping now runs.

--- spherical_spectral_element/theta_l/time_stepping.py
+++ spherical_spectral_element/theta_l/time_stepping.py
@@ -1,73 +1,87 @@
 from ..config import jnp
+from .infra import exit_codes, err_code
 from .model_state import wrap_model_struct, dss_model_state, wrap_tracer_avg_struct
-from .explicit_terms import explicit_tendency, lower_boundary_correction
+from .explicit_terms import explicit_tendency, correct_state
 from .hyperviscosity import hypervis_terms
 
 
 def rfold_state(state1, state2, fold_coeff1, fold_coeff2):
-  return wrap_model_struct(state1["u"] * fold_coeff1 + state2("u") * fold_coeff2,
-                           state1["vtheta_dpi"] * fold_coeff1 + state2("vtheta_dpi") * fold_coeff2,
-                           state1["dpi"] * fold_coeff1 + state2["vtheta_dpi"] * fold_coeff2,
+  return wrap_model_struct(state1["u"] * fold_coeff1 + state2["u"] * fold_coeff2,
+                           state1["vtheta_dpi"] * fold_coeff1 + state2["vtheta_dpi"] * fold_coeff2,
+                           state1["dpi"] * fold_coeff1 + state2["dpi"] * fold_coeff2,
                            state1["phi_surf"],
                            state1["grad_phi_surf"],
                            state1["phi_i"] * fold_coeff1 + state2["phi_i"] * fold_coeff2,
                            state1["w_i"] * fold_coeff1 + state2["w_i"] * fold_coeff2)
 
 
-def accumulate_avg_explicit_terms(averaging_weight, state_c0, tracer_struct):
-  return wrap_tracer_avg_struct(tracer_struct["avg_u"] + averaging_weight *
-                                state_c0["u"] *
-                                state_c0["dpi"][:, :, :, :, jnp.newaxis],
-                                tracer_struct["avg_dpi"],
-                                tracer_struct["avg_dpi_dissip"])
+#def accumulate_avg_explicit_terms(averaging_weight, state_c0, tracer_struct):
+#  return wrap_tracer_avg_struct(tracer_struct["avg_u"] + averaging_weight *
+#                                state_c0["u"] *
+#                                state_c0["dpi"][:, :, :, :, jnp.newaxis],
+#                                tracer_struct["avg_dpi"],
+#                                tracer_struct["avg_dpi_dissip"])
 
 
 def advance_state(states, coeffs):
-  for coeff_idx in len(states) - 1:
-    state_out = rfold_state(states[coeff_idx],
-                            states[coeff_idx + 1],
-                            coeffs[coeff_idx],
-                            coeffs[coeff_idx + 1])
+  state_out = rfold_state(states[0],
+                            states[1],
+                            coeffs[0],
+                            coeffs[1])
+  for coeff_idx in range(2, len(states)):
+    state_out = rfold_state(state_out,
+                            states[coeff_idx],
+                            1.0,
+                            coeffs[coeff_idx])
   return state_out
 
+def check_nan(state):
+  is_nan = False
+  for field in ["u", "vtheta_dpi", "dpi", "w_i", "phi_i"]:
+    is_nan = is_nan or jnp.any(jnp.isnan(state[field]))
+  return err_code("Nan encountered in time stepping") if is_nan else exit_codes["success"]
 
 def advance_euler(state_in, dt, h_grid, v_grid, config, dims, hydrostatic=True, deep=False):
   u_tend = explicit_tendency(state_in, h_grid, v_grid, config, hydrostatic=hydrostatic, deep=deep)
   u_tend_c0 = dss_model_state(u_tend, h_grid, dims, hydrostatic=hydrostatic)
   u1 = advance_state([state_in, u_tend_c0], [1.0, dt])
-  return lower_boundary_correction(state_in, u1, dt, h_grid, v_grid, dims, config, hydrostatic=hydrostatic, deep=deep)
+  u1_cons = correct_state(u1, dt, config, hydrostatic=hydrostatic, deep=deep)
+  return u1_cons, check_nan(u1_cons)
 
 
 def advance_euler_hypervis(state_in, dt, h_grid, v_grid, config, dims, ref_state, n_subcycle=1, hydrostatic=True):
-  hypervis_rhs = hypervis_terms(state_in, ref_state,
-                                h_grid, dims,
-                                config,
-                                hydrostatic=hydrostatic)
-  
+  state_out = state_in
+  for _ in range(n_subcycle):
+    hypervis_rhs = hypervis_terms(state_in, ref_state,
+                                  h_grid, dims,
+                                  config,
+                                  hydrostatic=hydrostatic)
+    state_out = advance_state([state_in, hypervis_rhs], [1.0, dt/n_subcycle])  
+  return state_out, check_nan(state_out)
 
 
 def ullrich_5stage(state_in, dt, h_grid, v_grid, config, dims, hydrostatic=True, deep=False):
-  u_tend = explicit_tendency(state_in, h_grid, v_grid, config, dims, hydrostatic=hydrostatic, deep=deep)
+  u_tend = explicit_tendency(state_in, h_grid, v_grid, config, hydrostatic=hydrostatic, deep=deep)
   u_tend_c0 = dss_model_state(u_tend, h_grid, dims, hydrostatic=hydrostatic)
   u1 = advance_state([state_in, u_tend_c0], [1.0, dt / 5.0])
-  u1 = lower_boundary_correction(state_in, u1, dt / 5.0, h_grid, v_grid, hydrostatic=hydrostatic, deep=deep)
-  u_tend = explicit_tendency(u1, h_grid, v_grid, config, dims, hydrostatic=hydrostatic, deep=deep)
+  u1 = correct_state(u1, dt / 5.0, config, hydrostatic=hydrostatic, deep=deep)
+  u_tend = explicit_tendency(u1, h_grid, v_grid, config, hydrostatic=hydrostatic, deep=deep)
   u_tend_c0 = dss_model_state(u_tend, h_grid, dims, hydrostatic=hydrostatic)
   u2 = advance_state([state_in, u_tend_c0], [1.0, dt / 5.0])
-  u2 = lower_boundary_correction(state_in, u2, dt / 5.0, h_grid, v_grid, config, dims,
-                                 hydrostatic=hydrostatic, deep=deep)
-  u_tend = explicit_tendency(u2, h_grid, v_grid, config, dims, hydrostatic=hydrostatic, deep=deep)
+  u2 = correct_state(u2, dt / 5.0, config, hydrostatic=hydrostatic, deep=deep)
+  u_tend = explicit_tendency(u2, h_grid, v_grid, config, hydrostatic=hydrostatic, deep=deep)
   u_tend_c0 = dss_model_state(u_tend, h_grid, dims, hydrostatic=hydrostatic)
   u3 = advance_state([state_in, u_tend_c0], [1.0, dt / 3.0])
-  u3 = lower_boundary_correction(state_in, u3, dt / 3.0, h_grid, v_grid, config, dims,
-                                 hydrostatic=hydrostatic, deep=deep)
-  u_tend = explicit_tendency(u3, h_grid, v_grid, config, dims, hydrostatic=hydrostatic, deep=deep)
+  u3 = correct_state(u3, dt / 3.0, config, hydrostatic=hydrostatic, deep=deep)
+  u_tend = explicit_tendency(u3, h_grid, v_grid, config, hydrostatic=hydrostatic, deep=deep)
   u_tend_c0 = dss_model_state(u_tend, h_grid, dims, hydrostatic=hydrostatic)
   u4 = advance_state([state_in, u_tend_c0], [1.0, 2.0 * dt / 3.0])
-  u4 = lower_boundary_correction(state_in, u4, 2.0 * dt / 3.0, h_grid, v_grid, config, dims,
-                                 hydrostatic=hydrostatic, deep=deep)
-  u_tend = explicit_tendency(u4, h_grid, v_grid, config, dims, hydrostatic=hydrostatic)
+  u4 = correct_state(u4, 2.0 * dt / 3.0, config, hydrostatic=hydrostatic, deep=deep)
+  u_tend = explicit_tendency(u4, h_grid, v_grid, config, hydrostatic=hydrostatic)
   u_tend_c0 = dss_model_state(u_tend, h_grid, dims, hydrostatic=hydrostatic)
-  return advance_state([state_in, u1, u_tend_c0], [-1.0 / 4.0,
+  
+  final_state = advance_state([state_in, u1, u_tend_c0], [-1.0 / 4.0,
                                                    5.0 / 4.0,
                                                    3.0 * dt / 4.0])
+  final_state = correct_state(final_state, 2.0 * dt / 3.0, config, hydrostatic=hydrostatic, deep=deep)
+  return final_state, check_nan(final_state)
