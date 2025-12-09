cab1f1bff534906926edf461c2ffc0337a58f418
Owen Hughes <okayhughes@gmail.com>
2025-12-09 12:04:01 -0500

    Add helper function to get dpi_ref from vertical coordinate.

--- spherical_spectral_element/theta_l/vertical_coordinate.py
+++ spherical_spectral_element/theta_l/vertical_coordinate.py
@@ -14,6 +14,12 @@ def mass_from_coordinate_midlev(ps, v_grid):
   return (v_grid["reference_pressure"] * v_grid["hybrid_a_m"][jnp.newaxis, jnp.newaxis, jnp.newaxis, :] +
           v_grid["hybrid_b_m"][jnp.newaxis, jnp.newaxis, jnp.newaxis, :] * ps[:, :, :, jnp.newaxis])
 
+def dmass_from_coordinate(ps, v_grid):
+  da = (v_grid["hybrid_a_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, 1:] - v_grid["hybrid_a_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, :-1])
+  db = (v_grid["hybrid_b_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, 1:] - v_grid["hybrid_b_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, :-1])
+  return (v_grid["reference_pressure"] * da +
+          db * ps[:, :, :, jnp.newaxis])
+
 
 def mass_from_coordinate_interface(ps, v_grid):
   return (v_grid["reference_pressure"] * v_grid["hybrid_a_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, :] +
