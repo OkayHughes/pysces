e228c9844c86f0be9931d630f5f1d0637dfa8171
Owen Hughes <okayhughes@gmail.com>
2025-12-09 20:39:01 -0500

    Lint vertical coordinate

--- spherical_spectral_element/theta_l/vertical_coordinate.py
+++ spherical_spectral_element/theta_l/vertical_coordinate.py
@@ -9,16 +9,22 @@ def create_vertical_grid(hybrid_a_i, hybrid_b_i, p0):
   v_grid["hybrid_b_m"] = 0.5 * (hybrid_b_i[1:] + hybrid_b_i[:-1])
   return v_grid
 
+
 @jit
 def mass_from_coordinate_midlev(ps, v_grid):
   return (v_grid["reference_pressure"] * v_grid["hybrid_a_m"][jnp.newaxis, jnp.newaxis, jnp.newaxis, :] +
           v_grid["hybrid_b_m"][jnp.newaxis, jnp.newaxis, jnp.newaxis, :] * ps[:, :, :, jnp.newaxis])
+
+
 @jit
 def dmass_from_coordinate(ps, v_grid):
-  da = (v_grid["hybrid_a_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, 1:] - v_grid["hybrid_a_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, :-1])
-  db = (v_grid["hybrid_b_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, 1:] - v_grid["hybrid_b_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, :-1])
+  da = (v_grid["hybrid_a_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, 1:] -
+        v_grid["hybrid_a_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, :-1])
+  db = (v_grid["hybrid_b_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, 1:] -
+        v_grid["hybrid_b_i"][jnp.newaxis, jnp.newaxis, jnp.newaxis, :-1])
   return (v_grid["reference_pressure"] * da +
           db * ps[:, :, :, jnp.newaxis])
+
 
 @jit
 def mass_from_coordinate_interface(ps, v_grid):
