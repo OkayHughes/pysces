28683e7bfacd0aae488cace80730a34c080392c1
Owen Hughes <okayhughes@gmail.com>
2025-12-29 16:17:12 -0500

    Regression test script, first attempt at.

---
+++ run_tests.sh
@@ -0,0 +1,6 @@
+uv run pysces/set_config.py --wrapper_type none
+uv run pytest tests
+uv run pysces/set_config.py --wrapper_type jax
+uv run --group jax pytest tests
+uv run pysces/set_config.py --wrapper_type torch
+uv run --group torch pytest tests
