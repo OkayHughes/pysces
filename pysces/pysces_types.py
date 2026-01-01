c0b630852ca1d203f077255ca3d50f1349ba1cd5
Owen Hughes <okayhughes@gmail.com>
2026-01-01 14:54:19 -0500

    Disable types for doc generation.

--- pysces/pysces_types.py
+++ pysces/pysces_types.py
@@ -1,17 +1,19 @@
 from typing import Dict, NewType
-from jaxtyping import Array, Float, Int64
+import numpy as np
+# Float = np.dtype[np.float64]
+# Int64 = np.dtype[np.int64]
+# Array = np.ndarray
+# ProcIdx = NewType("ProcIdx", int)
 
-ProcIdx = NewType("ProcIdx", int)
+# ElemIdx = NewType("ElemIdx", int)
+# ElemIdxLocal = NewType("ElemIdxLocal", ElemIdx)
+# ElemIdxGlobal = NewType("ElemIdxGlobal", ElemIdx)
+# GllIdx = NewType("GllIdx", int)
+# GllIdxI = NewType("GllIdxI", GllIdx)
+# GllIdxJ = NewType("GllIdxJ", GllIdx)
 
-ElemIdx = NewType("ElemIdx", int)
-ElemIdxLocal = NewType("ElemIdxLocal", ElemIdx)
-ElemIdxGlobal = NewType("ElemIdxGlobal", ElemIdx)
-GllIdx = NewType("GllIdx", int)
-GllIdxI = NewType("GllIdxI", GllIdx)
-GllIdxJ = NewType("GllIdxJ", GllIdx)
+# VertRedundancyLocal = Dict[ElemIdxLocal, Dict[tuple[GllIdxI, GllIdxJ], list[tuple[ElemIdxLocal, GllIdxI, GllIdxJ]]]]
+# VertRedundancyRemote = Dict[ProcIdx, list[tuple[ElemIdxLocal, GllIdxI, GllIdxJ]]]
 
-VertRedundancyLocal = Dict[ElemIdxLocal, Dict[tuple[GllIdxI, GllIdxJ], list[tuple[ElemIdxLocal, GllIdxI, GllIdxJ]]]]
-VertRedundancyRemote = Dict[ProcIdx, list[tuple[ElemIdxLocal, GllIdxI, GllIdxJ]]]
-
-AssemblyTriple = tuple[Float[Array, "point_idx"], Int64[Array, "point_idx"], Int64[Array, "point_idx"]]
-Decomp = list[tuple[int]]
+# AssemblyTriple = tuple[Float[Array, "point_idx"], Int64[Array, "point_idx"], Int64[Array, "point_idx"]]
+# Decomp = list[tuple[int]]
