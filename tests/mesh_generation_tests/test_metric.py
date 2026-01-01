d7df4bbbc0289fe418bdc38fe32082e2ddcece46
Owen Hughes <okayhughes@gmail.com>
2025-12-31 21:22:19 -0500

    Docstrings for equiangular_metric.

--- tests/mesh_generation_tests/test_metric.py
+++ tests/mesh_generation_tests/test_metric.py
@@ -12,7 +12,7 @@ def test_gen_metric():
       vert_redundancy = gen_vert_redundancy(nx, face_connectivity, face_position)
       gll_position, gll_jacobian = mesh_to_cart_bilinear(face_position_2d, npt)
       cube_redundancy = gen_gll_redundancy(face_connectivity, vert_redundancy, npt)
-      gll_latlon, cube_to_sphere_jacobian = gen_metric_terms_equiangular(face_mask, gll_position, cube_redundancy, npt)
+      gll_latlon, cube_to_sphere_jacobian = gen_metric_terms_equiangular(face_mask, gll_position, npt)
       for elem_idx in cube_redundancy.keys():
         for (i_idx, j_idx) in cube_redundancy[elem_idx].keys():
           for elem_idx_pair, i_idx_pair, j_idx_pair in cube_redundancy[elem_idx][(i_idx, j_idx)]:
@@ -25,7 +25,7 @@ def test_gen_mass_mat():
     for nx in [14, 15]:
       face_connectivity, face_mask, face_position, face_position_2d = gen_cube_topo(nx)
       vert_redundancy = gen_vert_redundancy(nx, face_connectivity, face_position)
-      grid, dims = gen_metric_from_topo(face_connectivity, face_mask, face_position_2d, vert_redundancy, npt, jax=False)
+      grid, dims = gen_metric_from_topo(face_connectivity, face_mask, face_position_2d, vert_redundancy, npt, wrapped=False)
       assert (np.allclose(np.sum(grid["met_det"] *
                                  (grid["gll_weights"][np.newaxis, :, np.newaxis] *
                                   grid["gll_weights"][np.newaxis, np.newaxis, :])), 4 * np.pi))
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,7 @@
 uv run ../pysces/set_config.py --wrapper_type none
-uv run pytest
+uv run pytest $1
 uv run ../pysces/set_config.py --wrapper_type jax
-uv run --group jax pytest
+uv run --group jax pytest $1
 uv run ../pysces/set_config.py --wrapper_type torch
-uv run --group torch pytest
+uv run --group torch pytest $1
 uv run ../pysces/set_config.py
