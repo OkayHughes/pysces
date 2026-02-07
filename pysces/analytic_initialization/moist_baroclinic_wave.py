e8668882d45587479b25526ea0254a7b91206dc6
Owen Hughes <okayhughes@gmail.com>
2026-02-07 16:22:59 -0500

    Docstrings for UMJS tests

--- pysces/analytic_initialization/moist_baroclinic_wave.py
+++ pysces/analytic_initialization/moist_baroclinic_wave.py
@@ -29,7 +29,7 @@ def init_baroclinic_wave_config(T0E=310,
                                 moistE0Ast=610.78,
                                 p0=1e5,
                                 radius_earth=6371e3,
-                                period_earth=7.292e-5,
+                                angular_freq_earth=7.292e-5,
                                 Rgas=287.1,
                                 R_water_vapor=461.50,
                                 gravity=9.81,
@@ -44,32 +44,91 @@ def init_baroclinic_wave_config(T0E=310,
                                 mountain_lon_widths=[7.0 * jnp.pi / 180.0,
                                                      7.0 * jnp.pi / 180.0]):
   """
-  [Description]
+  Create a struct containing all parameters necessary to initialize the 
+  moist/dry baroclinic wave 
 
   Parameters
   ----------
-  [first] : array_like
-      the 1st param name `first`
-  second :
-      the 2nd param
-  third : {'value', 'other'}, optional
-      the 3rd param, by default 'value'
+  T0E : float
+    The nominal value for surface temperature at the equator
+  T0P : float
+    The nominal value for the surface temperature at the pole
+  B : float
+    Vertical jet width parameter
+  K : float
+    Horizontal jet width parameter
+  lapse : float
+    Nominal atmospheric lapse rate at the surface
+  pertu0 : float
+    Streamfunction perturbation amplitude
+  pertr : float
+    Streamfunction perturbation radius
+  pertup: float
+    Exponential perturbation amplitude
+  pertexpr: float
+    Exponential perturbation width parameter
+  pertlon : float
+    Longitude of perturbation (either type) in radians.
+  pertlat : float
+    Latitude of perturbation (either type) in radians.
+  pertz : float
+    Maximum height of perturbation in height coordinates (m)
+  moistqlat : float
+    Meridional width parameter of moisture in radians
+  moistqp : float
+    Vertical width parameter of moisture in pressure units (Pa)
+  moisttr : float
+    Vertical cutoff for moisture (ignored for the moment)
+  moistq0 : float
+    Maximum specific humidity amplitude (moist mixing ratio, fraction not percentage)
+  p0 : float
+    Nominal constant thermodynamic moist surface pressure.
+  radius_earth : float
+    Nominal radius of the planet surface (m), `a` or `rearth` in most ESMs
+  angular_freq_earth : float
+    Angular frequency of the earth (1 / sec), `omega` in most ESMs
+  Rgas : float
+    Nominal gas constant of dry air (J kg^-1 K^-1)
+  R_water_vapor : float
+    Gas constant for water vapor (J kg^-1 K^-1)
+  gravity : float
+    Nominal constant strength of gravity at the surface (m s^-2)
+  model_config :
+    physics_config struct for your dynamical core. This allows
+    other coefficients like Rgas to be read directly from your 
+    simulation configuration.
+  alpha : float
+    Skamarock, Ong, and Klemp correction for small-planet simulations.
+  mountain_heights : list[float]
+    Heights of mountains if present
+  mountain_lons : list[float]
+    Longitudinal position of mountain centers (radians)
+  mountain_lats : list[float]
+    Latitudinal position of mountain centers (radians)
+  mountain_lat_widths : list[float]
+    Distance (radians) between where surface height is 10% of its longitude-wise maximum
+  mountain_lon_widths : list[float]
+    Distance (radians) between where surface height is 10% of its latitude-wise maximum
+
+  Notes
+  -----
+  The parameters here cover several test cases. The base state is derived in
+  https://doi.org/10.1002/qj.2018 and developed into a baroclinic wave test
+  case in https://doi.org/10.1002/qj.2241. Moisture was added for DCMIP2016,
+  and a topographic variant was developed in https://doi.org/10.5194/gmd-16-6805-2023
+
 
   Returns
   -------
-  string
-      a value in a string
+  dict[str, Any]
+      test_config to be passed to other functions in this.
 
-  Raises
-  ------
-  KeyError
-      when a key error
   """
   moistqs = 1e-12
   dx_epsilon = 1e-5
   if model_config:
     radius_earth = model_config["radius_earth"]
-    period_earth = model_config["period_earth"]
+    angular_freq_earth = model_config["angular_freq_earth"]
     Rgas = model_config["Rgas"]
     R_water_vapor = model_config["moisture_species_Rgas"]["water_vapor"]
     gravity = model_config["gravity"]
@@ -101,7 +160,7 @@ def init_baroclinic_wave_config(T0E=310,
           "moistE0Ast": device_wrapper(moistE0Ast),
           "p0": device_wrapper(p0),
           "radius_earth": device_wrapper(radius_earth),
-          "period_earth": device_wrapper(period_earth),
+          "angular_freq_earth": device_wrapper(angular_freq_earth),
           "Rgas": device_wrapper(Rgas),
           "R_water_vapor": device_wrapper(R_water_vapor),
           "gravity": device_wrapper(gravity),
@@ -114,45 +173,45 @@ def init_baroclinic_wave_config(T0E=310,
           }
 
 
-def eval_T0(config):
+def _eval_T0(config):
   return (config["alpha"] * config["T0E"] +
           (1.0 - config["alpha"]) * config["T0P"])
 
 
-def eval_constH(config):
-  return config["Rgas"] * eval_T0(config) / config["gravity"]
+def _eval_constH(config):
+  return config["Rgas"] * _eval_T0(config) / config["gravity"]
 
 
-def eval_constA(config):
+def _eval_constA(config):
   return 1.0 / config["lapse"]
 
 
-def eval_constB(config):
-  T0 = eval_T0(config)
+def _eval_constB(config):
+  T0 = _eval_T0(config)
   return (T0 - config["T0P"]) / (T0 * config["T0P"])
 
 
-def eval_scaledZ(z,
-                 config):
-  return z / (config["B"] * eval_constH(config))
+def _eval_scaledZ(z,
+                  config):
+  return z / (config["B"] * _eval_constH(config))
 
 
-def eval_inttau2(z,
-                 config):
-  return (eval_constC(config) * z *
-          jnp.exp(-eval_scaledZ(z, config)**2))
+def _eval_inttau2(z,
+                  config):
+  return (_eval_constC(config) * z *
+          jnp.exp(-_eval_scaledZ(z, config)**2))
 
 
-def eval_constC(config):
+def _eval_constC(config):
   T0E = config["T0E"]
   T0P = config["T0P"]
   return (0.5 * (config["K"] + 2.0) *
           (T0E - T0P) / (T0E * T0P))
 
 
-def eval_r_hat(z,
-               config,
-               deep=False):
+def _eval_r_hat(z,
+                config,
+                deep=False):
   # note: should be separate from model code.
   # so constant-g equation set can be used
   if deep:
@@ -167,26 +226,28 @@ def eval_z_surface(lat,
                    config,
                    mountain=False):
   """
-  [Description]
+  Evaluate surface height (m)
 
   Parameters
   ----------
-  [first] : array_like
-      the 1st param name `first`
-  second :
-      the 2nd param
-  third : {'value', 'other'}, optional
-      the 3rd param, by default 'value'
+  lat : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+    Latitude (radians)
+  lon : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+    Longitude (radians)
+  config : TestConfig
+    Dict-like containing parameters for test case
+  mountain : bool, default=False
+    If true, use the surface height from the topographic version of the test case.
+
+  Notes
+  -----
+  * See init_baroclinic_wave_config for how to initialize config.
+  * The topography is described in https://doi.org/10.5194/gmd-16-6805-2023
 
   Returns
   -------
-  string
-      a value in a string
-
-  Raises
-  ------
-  KeyError
-      when a key error
+  z_surf : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+      Surface height (m)
   """
   if mountain:
     zs = jnp.zeros_like(lat)
@@ -213,34 +274,37 @@ def eval_pressure_temperature(z,
                               config,
                               deep=False):
   """
-  [Description]
+  Evaluate pressure and (dry) temperature on a 3d grid.
 
   Parameters
   ----------
-  [first] : array_like
-      the 1st param name `first`
-  second :
-      the 2nd param
-  third : {'value', 'other'}, optional
-      the 3rd param, by default 'value'
+  z : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Geometric height above the model surface in meters
+  lat : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+      Latitude (radians)
+  config : TestConfig
+    Dict-like containing parameters for test case
+  deep : bool, default=False
+    If true, use the deep atmopsphere base state.
+
+  Notes
+  -----
+  * See init_baroclinic_wave_config for how to initialize config.
 
   Returns
   -------
-  string
-      a value in a string
-
-  Raises
-  ------
-  KeyError
-      when a key error
+  pressure : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Moist pressure (Pa)
+  temperature : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Temperature (not virtual), in Kelvin
   """
   lapse = config["lapse"]
   K = config["K"]
-  T0 = eval_T0(config)
-  constA = eval_constA(config)
-  constB = eval_constB(config)
-  constC = eval_constC(config)
-  scaledZ = eval_scaledZ(z, config)
+  T0 = _eval_T0(config)
+  constA = _eval_constA(config)
+  constB = _eval_constB(config)
+  constC = _eval_constC(config)
+  scaledZ = _eval_scaledZ(z, config)
 
   # note: this can be optimized for numpy so
   # scaledZ**2 quantities are not recomputed
@@ -251,9 +315,9 @@ def eval_pressure_temperature(z,
 
   inttau1 = (constA * (jnp.exp(lapse * z / T0) - device_wrapper(1.0)) +
              constB * z * jnp.exp(-scaledZ**2))
-  inttau2 = eval_inttau2(z, config)
+  inttau2 = _eval_inttau2(z, config)
 
-  r_hat = eval_r_hat(z, config, deep=deep)
+  r_hat = _eval_r_hat(z, config, deep=deep)
 
   inttermT = ((r_hat * jnp.cos(lat)[:, :, :, np.newaxis])**K -
               K / (K + 2.0) * (r_hat * jnp.cos(lat)[:, :, :, np.newaxis])**(K + 2))
@@ -270,26 +334,32 @@ def eval_surface_state(lat,
                        deep=False,
                        mountain=False):
   """
-  [Description]
+  Evaluate the surface height and moist pressure.
 
   Parameters
   ----------
-  [first] : array_like
-      the 1st param name `first`
-  second :
-      the 2nd param
-  third : {'value', 'other'}, optional
-      the 3rd param, by default 'value'
+  lat : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+      Latitude (radians)
+  lon : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+      Longitude(radians)
+  config : TestConfig
+    Dict-like containing parameters for test case
+  deep : bool
+    If true, use steady state for deep atmosphere equation set
+  mountain : bool
+    If true, use surface topography from the mountain version of the test.
+
+  Notes
+  -----
+  * See init_baroclinic_wave_config for how to initialize config.
 
   Returns
   -------
-  string
-      a value in a string
+  z_surface : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Surface height in meters
+  p_surface : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Moist surface pressure in Pascal.
 
-  Raises
-  ------
-  KeyError
-      when a key error
   """
   z_surface = eval_z_surface(lat, lon, config, mountain=mountain)
   p_surface = eval_pressure_temperature(z_surface[:, :, :, np.newaxis],
@@ -307,30 +377,51 @@ def eval_state(lat,
                moist=False,
                pert_type=perturbation_opts.none):
   """
-  [Description]
+  Calculate zonal wind, meridional wind, pressure, virtual temperature, and moisture vapor.
 
   Parameters
   ----------
-  [first] : array_like
-      the 1st param name `first`
-  second :
-      the 2nd param
-  third : {'value', 'other'}, optional
-      the 3rd param, by default 'value'
+  lat : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+      Latitude (radians)
+  lon : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+      Longitude (radians)
+  z : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Geometric height above the model surface in meters
+  config : TestConfig
+    Dict-like containing parameters for test case
+  deep : bool
+    If true, use steady state for deep-atmosphere equation set.
+  moist : bool
+    If true, use idealized moisture profile that can be used with Kessler physics.
+  pert_type : perturbation_opts
+    Type of perturbation to be used
+
+  Notes
+  -----
+  * See init_baroclinic_wave_config for how to initialize config.
+  * Valid perturbation types are
+    * perturbation_opts.exponential: (used in DCMIP 2016 cases). This is a little noisier,
+    but avoids the dycore-specific model spread that the streamfunction perturbation causes.
+    * erturbation_opts.streamfunction: the perturbation used in the original dry test case paper.
+    A less divergent perturbation that avoids initialization noise.
+    * none: use the analytic steady state. Use this for steady state tests.
 
   Returns
   -------
-  string
-      a value in a string
-
-  Raises
-  ------
-  KeyError
-      when a key error
+  u : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Zonal wind (m s^-1)
+  v : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Meridional wind (m s^-1)
+  pressure : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Moist pressure (Pa)
+  virtual_temperature : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Virtual temperature (K)
+  q : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Specific humidity (kg moisture / kg moist air, fraction not percentage)
   """
   K = config["K"]
-  inttau2 = eval_inttau2(z, config)
-  r_hat = eval_r_hat(z, config, deep=deep)
+  inttau2 = _eval_inttau2(z, config)
+  r_hat = _eval_r_hat(z, config, deep=deep)
   cos_lat = jnp.cos(lat)[:, :, :, np.newaxis]
   inttermU = ((r_hat * cos_lat)**(K - 1.0) -
               (r_hat * cos_lat)**(K + 1.0))
@@ -342,7 +433,7 @@ def eval_state(lat,
     rcoslat = config["radius_earth"] * cos_lat
   else:
     rcoslat = (z + config["radius_earth"]) * cos_lat
-  solid_body_rotation = config["period_earth"] * rcoslat
+  solid_body_rotation = config["angular_freq_earth"] * rcoslat
   u = -solid_body_rotation + jnp.sqrt(solid_body_rotation**2 +
                                       rcoslat * bigU)
   v = jnp.zeros_like(u)
@@ -375,26 +466,24 @@ def eval_great_circle_dist(lat,
                            lon,
                            config):
   """
-  [Description]
+  Evaluate the great circle distance in radians from the perturbation center
 
   Parameters
   ----------
-  [first] : array_like
-      the 1st param name `first`
-  second :
-      the 2nd param
-  third : {'value', 'other'}, optional
-      the 3rd param, by default 'value'
+  lat : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+    Latitude (radians)
+  lon : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+    Longitude (radians)
+  config : TestConfig
+    Dict-like containing parameters for test case
+
+  Notes
+  -----
+  * See init_baroclinic_wave_config for how to initialize config.
 
   Returns
   -------
-  string
-      a value in a string
-
-  Raises
-  ------
-  KeyError
-      when a key error
+  gc_dist : Array[Float, tuple[elem_idx, i_idx, j_idx]]
   """
   return (1.0 / config["pertexpr"] *
           jnp.arccos(jnp.sin(config["pertlat"]) *
@@ -407,26 +496,23 @@ def eval_great_circle_dist(lat,
 def eval_taper_fn(z,
                   config):
   """
-  [Description]
+  Evaluate the vertical taper function used by both perturbation types
 
   Parameters
   ----------
-  [first] : array_like
-      the 1st param name `first`
-  second :
-      the 2nd param
-  third : {'value', 'other'}, optional
-      the 3rd param, by default 'value'
+  z : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Geometric height above the model surface in meters
+  config : TestConfig
+    Dict-like containing parameters for test case
+
+  Notes
+  -----
+  * See init_baroclinic_wave_config for how to initialize config.
 
   Returns
   -------
-  string
-      a value in a string
-
-  Raises
-  ------
-  KeyError
-      when a key error
+  taper : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Unitless taper function that controls vertical structure of perturbation.
   """
   pertz = config["pertz"]
   taper_below_pertz = (1.0 - 3.0 * z**2 / pertz**2 + 2.0 * z**3 / pertz**3)
@@ -440,26 +526,27 @@ def eval_exponential(lat,
                      z,
                      config):
   """
-  [Description]
+  Evaluate exponential perturbation for zonal wind field
 
   Parameters
   ----------
-  [first] : array_like
-      the 1st param name `first`
-  second :
-      the 2nd param
-  third : {'value', 'other'}, optional
-      the 3rd param, by default 'value'
+  lat : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+    Latitude (radians)
+  lon : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+    Longitude (radians)
+  z : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Geometric height above the model surface in meters
+  config : TestConfig
+    Dict-like containing parameters for test case
+
+  Notes
+  -----
+  * See init_baroclinic_wave_config for how to initialize config.
 
   Returns
   -------
-  string
-      a value in a string
-
-  Raises
-  ------
-  KeyError
-      when a key error
+  u_perturbation : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Perturbation in m s^-1
   """
   greatcircle_dist = eval_great_circle_dist(lat, lon, config)[:, :, :, np.newaxis]
   taper = eval_taper_fn(z, config)
@@ -477,26 +564,30 @@ def eval_streamfunction(lat,
                         z,
                         config):
   """
-  [Description]
+  Evaluate streamfunction from which perturbation can be computed.
 
   Parameters
   ----------
-  [first] : array_like
-      the 1st param name `first`
-  second :
-      the 2nd param
-  third : {'value', 'other'}, optional
-      the 3rd param, by default 'value'
+  lat : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+    Latitude (radians)
+  lon : Array[Float, tuple[elem_idx, i_idx, j_idx]]
+    Longitude (radians)
+  z : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Geometric height above the model surface in meters
+  config : TestConfig
+    Dict-like containing parameters for test case
+
+  Notes
+  -----
+  * See init_baroclinic_wave_config for how to initialize config.
+  * The resulting u, v values are typically computed by computing finite differences:
+    * u = -∂ψ/∂ϕ
+    * v = ∂ψ/∂λ
 
   Returns
   -------
-  string
-      a value in a string
-
-  Raises
-  ------
-  KeyError
-      when a key error
+  streamfunction : Array[Float, tuple[elem_idx, i_idx, j_idx, level_idx]]
+    Streamfunction in m s^-1 (this is a mathematician's streamfunction).
   """
   greatcircle_dist = eval_great_circle_dist(lat, lon, config)
   taper = eval_taper_fn(z, config)
